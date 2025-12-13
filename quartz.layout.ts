5431b32148e488fd14d076816d2f1bf3069a3bf3
sebastianpor5@gmail.com <psebastian115@gmail.com>
2025-12-13 14:15:10 +0100

    Quartz sync: Dec 13, 2025, 2:15 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -24,6 +24,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
+	Component.TableOfContents(),
   ],
   left: [
     Component.PageTitle(),
